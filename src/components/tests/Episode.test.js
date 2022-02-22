add5573ce11bb99f4ede3182941d0b6fc6f20e4d
Victor Gordian <victorgordian103@gmail.com>
2022-02-21 21:48:04 -0800

    first suite done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,40 @@ import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import Episode from './../Episode';
 
-test("renders without error", () => { });
+const testEp = { 
+    id: 1 , 
+    name: "", 
+    season: 1,
+    image:null, 
+    number: 1, 
+    summary: "test stuff", 
+    runtime: 1 
+    };
+    test("renders without error", () => { 
+    render(<Episode episode={testEp}/>)
+});
 
-test("renders the summary test passed as prop", () => { });
+const testWithoutImage = { 
+    id: 1 , 
+    name: "", 
+    season: 1, 
+    number: 1, 
+    image:null,
+    summary: "test stuff", 
+    runtime: 1 
+    }
 
-test("renders default image when image is not defined", () => { });
+test("renders the summary test passed as prop", () => { 
+    render(<Episode episode={testEp}/>);
+    const summary = screen.queryByText(/test stuff/i);
+
+    expect(summary).toBeInTheDocument();
+    expect(summary).toBeTruthy();
+    expect(summary).toHaveTextContent("test stuff")
+});
+
+test("renders default image when image is not defined", () => { 
+    render(<Episode episode={testWithoutImage}/>);
+    const img = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png');
+    expect(img).toBeInTheDocument();
+});
